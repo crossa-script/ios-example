35cc61a6d0058ee64db284d4844995e2500f6672
Yazan Tarifi <yazantarifi989@gmail.com>
2026-09-07 23:30:26 +0300

    Align iOS artifact with final Crossa source

--- CrossaIOSExample/Core/ExampleConfiguration.swift
+++ CrossaIOSExample/Core/ExampleConfiguration.swift
@@ -6,7 +6,7 @@ enum ExampleConfiguration {
     static let requestIterations = 5
     static let requestDelayNanoseconds: UInt64 = 2_000_000_000
     static let crossaArtifactChecksum = "e1d3fb6872bde28fe1a185ade80b97187366c9fbbefa4f3a3a049503a448d82c"
-    static let crossaSourceCommit = "e13bde22f4210f0d0d3e508c28f32464aba105bf"
+    static let crossaSourceCommit = "03d421801191f67c59722e96a1cc4a962a4b54ba"
     static let requestHeaders = [
         "Accept": "application/json",
         "X-Crossa-Demo": "ios-example",
